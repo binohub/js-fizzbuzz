9f3a914761cb24cc9483f24239bbea3693d77af3
binohub <yeebino001@gmail.com>
2022-06-16 16:12:53 +0200

    ms-1 - index, main & style - start

---
+++ main.js
@@ -0,0 +1,17 @@
+//! Scrivi un programma che stampi in console i numeri da 1 a 100
+
+// crea un ciclo che si ripete fino ad arrivare a 100 //?come faccio a partire da 1? condizione <= 100 
+    // stampa in console il numero //?come faccio a tenere il conto? stampa index 
+
+
+
+//! bonus
+
+// crea un ciclo che si ripete fino ad arrivare a 100 
+    // se il numero è divisibile per 3 stampa in console "fizz" //?come? dividi per 3 e dovrebbe darti 0 come resto
+    // se invece il numero è divisibile per 5 stampa in console "buzz" //?come? dividi per 5 e dovrebbe darti 0 come resto
+    // se invece il numero è divisibile per 15 stampa in console "fizzbuzz" //?come? dividi per 15 e dovrebbe darti 0 come resto
+    // altrimenti stampa in console il numero
+
+
+

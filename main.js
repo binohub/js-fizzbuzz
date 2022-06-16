ee92b4a3fcc9734676b415422bdd62bb4c0382e0
binohub <yeebino001@gmail.com>
2022-06-16 17:50:07 +0200

    ms2 - html output - start

--- main.js
+++ main.js
@@ -5,23 +5,41 @@
 //     console.log(index);  
 // }
 //! ms 1 & bonus
+
+const containerFizzBuzz = document.getElementById('container-fizzbuzz');
 // crea un ciclo da 1 a 100
 for (let index = 1; index <= 100; index++) {
-    
-    // se divisibile per 15 stampa in console "fizzbuzz"
+    // se divisibile per 15 stampa "fizzbuzz" //?come? crea un div, assegna le classi e il contenuto - da non dimenticare la variabile da collegare al rowcontainer
     if (index % 15 === 0) {
         console.log("FIZZBUZZ! (15)");
+        const divBox = document.createElement("div");
+        divBox.classList.add("col-2");
+        divBox.innerHTML = `FIZZBUZZ`;
+        containerFizzBuzz.append(divBox);
     }
-    // se divisibile per 5 stampa in console "buzz"
+    // se divisibile per 5 stampa "buzz"
     else if (index % 5 === 0) {
         console.log("BUZZ! (5)");
+        const divBox = document.createElement("div");
+        divBox.classList.add("col-2");
+        divBox.innerHTML = `BUZZ`;
+        containerFizzBuzz.append(divBox);
+
     }   
-    // se divisibile per 3 stampa in console "fizz" //?come? dividi per 3 e stampa solo se ti restituisce 0 come resto
+    // se divisibile per 3 stampa "fizz" //?come? dividi per 3 e stampa solo se ti restituisce 0 come resto
     else if (index % 3 === 0) {
         console.log("FIZZ! (3)"); 
+        const divBox = document.createElement("div");
+        divBox.classList.add("col-2");
+        divBox.innerHTML = "FIZZ";
+        containerFizzBuzz.append(divBox);
     }    
-    // altrimenti stampa in console il numero
+    // altrimenti stampa il numero
     else {
         console.log(index);
+        const divBox = document.createElement("div");
+        divBox.classList.add("col-2");
+        divBox.innerHTML = index;
+        containerFizzBuzz.append(divBox);
     }
 }

feefd00441d9a3470bda5b71f117cafa9a3957f3
binohub <yeebino001@gmail.com>
2022-06-16 16:50:26 +0200

    ms1 - for - finish

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 //! Scrivi un programma che stampi in console i numeri da 1 a 100
-// crea un ciclo da 1 a 100 //? come faccio a partire da 1? condizione <= 100
+// // crea un ciclo da 1 a 100 //? come faccio a partire da 1? condizione <= 100
+// for (let index = 1; index <= 100; index++) {
+//     // stampa in console il numero //?come faccio a tenere il conto? sfrutta l'index
+//     console.log(index);  
+// }
+//! ms 1 & bonus
+// crea un ciclo da 1 a 100
 for (let index = 1; index <= 100; index++) {
-    // stampa in console il numero //?come faccio a tenere il conto? sfrutta l'index
-    console.log(index);  
-}
-
-
-
-//! bonus
-
-// crea un ciclo che si ripete fino ad arrivare a 100 
-    // se il numero è divisibile per 3 stampa in console "fizz" //?come? dividi per 3 e dovrebbe darti 0 come resto
-    // se invece il numero è divisibile per 5 stampa in console "buzz" //?come? dividi per 5 e dovrebbe darti 0 come resto
-    // se invece il numero è divisibile per 15 stampa in console "fizzbuzz" //?come? dividi per 15 e dovrebbe darti 0 come resto
+    
+    // se divisibile per 15 stampa in console "fizzbuzz"
+    if (index % 15 === 0) {
+        console.log("FIZZBUZZ! (15)");
+    }
+    // se divisibile per 5 stampa in console "buzz"
+    else if (index % 5 === 0) {
+        console.log("BUZZ! (5)");
+    }   
+    // se divisibile per 3 stampa in console "fizz" //?come? dividi per 3 e stampa solo se ti restituisce 0 come resto
+    else if (index % 3 === 0) {
+        console.log("FIZZ! (3)"); 
+    }    
     // altrimenti stampa in console il numero
-
-
-
+    else {
+        console.log(index);
+    }
+}
